d7e43a677a0409c29dbcd21e3c48cd1b343757f2
vagrant <vagrant@nodejs-vagrant.(none)>
2014-05-01 18:37:04 +0000

    Update gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,48 +1,138 @@
 'use strict';
 
 module.exports = function(grunt) {
-  require('time-grunt')(grunt);
+  //require('time-grunt')(grunt);
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-html2js');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-nodemon');
 
   // Project Configuration
   grunt.initConfig({
+    distdir: 'public/build',
     pkg: grunt.file.readJSON('package.json'),
+    banner:
+    '/*!\n' +
+    ' * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+    ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+    ' * License <%= pkg.license %>\n */\n',
     assets: grunt.file.readJSON('server/config/assets.json'),
-    watch: {
-      js: {
-        files: ['gruntfile.js', 'server.js', 'server/**/*.js', '!public/vendor/*.js', 'public/**/*.js', 'test/**/*.js'],
-        tasks: ['jshint'],
+    clean: {
+      build: {
+        src: ['<%= distdir %>/*'],
         options: {
+          force: true
+        }
+      }
+    },
+    src: {
+      js: ['public/**/*.js', '!public/vendor/*'],
+      vendor: ['public/vendor/**/*.js'],
+      jsTpl: ['<%= distdir %>/templates/**/*.js'],
+      specs: ['test/protractor/**/*.spec.js'],
+      scenarios: ['test/protractor/**/*.scenario.js'],
+      html: ['src/index.html'],
+      tpl: {
+        app: ['public/**/*.tpl.html', '!public/vendor/*']
+      },
+      less: ['public/system/assets/less/*.less', 'public/**/assets/less/*.less'],
+      lessWatch: ['public/**/assets/less/*.less']
+    },
+    watch: {
+      server: {
+        files: ['server/**/*.js', 'server/views/**/*.html', 'gruntfile.js'],
+        options: {
+          reload: true,
           livereload: true
         }
       },
       html: {
-        files: ['public/**/views/*.html', 'server/views/**'],
+        files: ['<%= src.tpl.app %>'],
+        tasks: ['html2js'],
         options: {
           livereload: true
         }
       },
-      css: {
-        files: ['public/**/assets/css/*.css'],
-        tasks: ['csslint'],
+      less: {
+        files: ['<%= src.lessWatch %>'],
+        tasks: ['less:compileCore'],
         options: {
           livereload: true
         }
+      }
+    },
+    copy: {
+      fonts: {
+        files: [{ dest: '<%= distdir %>/fonts/', src : '**', expand: true, cwd: 'public/vendor/bootstrap/dist/fonts/' }]
       }
     },
     jshint: {
       all: {
-        src: ['gruntfile.js', 'server.js', 'server/**/*.js', '!public/vendor/*.js', 'public/**/*.js', 'test/**/*.js', '!test/coverage/**/*.js'],
-        options: {
+        src: ['gruntfile.js', 'server.js', 'server/**/*.js', '<%= assets.js %>', '!public/build/templates/app.js', 'test/**/*.js'], options: {
           jshintrc: true
         }
       }
     },
+    html2js: {
+      app: {
+        options: {
+          base: 'public'
+        },
+        src: ['<%= src.tpl.app %>'],
+        dest: '<%= distdir %>/templates/app.js',
+        module: 'templates.app'
+      }
+    },
+    concat:{
+      dist:{
+        options: {
+          banner: '<%= banner %>'
+        },
+        src:['<%= assets.app %>'],
+        dest:'<%= distdir %>/js/<%= pkg.name %>.js'
+      }
+    },
     uglify: {
       options: {
-        mangle: false
+        mangle: false,
+        banner: '<%= banner %>'
       },
       production: {
-        files: '<%= assets.js %>'
+        src: ['<%= assets.vendors %>', '<%= assets.js %>'],
+        dest:'<%= distdir %>/js/<%= pkg.name %>.min.js'
+      }
+    },
+    less: {
+      compileCore: {
+        options: {
+          strictMath: true,
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: '<%= pkg.name %>.css.map',
+          sourceMapFilename: '<%= distdir %>/css/<%= pkg.name %>.css.map'
+        },
+        files: {
+          '<%= distdir %>/css/<%= pkg.name %>.css': ['<%= src.less %>']
+        }
+      },
+      minify: {
+        options: {
+          cleancss: true,
+          report: 'min'
+        },
+        files: {
+          '<%= distdir %>/css/<%= pkg.name %>.css': '<%= src.less %>'
+        }
       }
     },
     csslint: {
@@ -53,21 +143,13 @@ module.exports = function(grunt) {
         src: ['public/**/assets/css/*.css']
       }
     },
-    cssmin: {
-      combine: {
-        files: '<%= assets.css %>'
-      }
-    },
     nodemon: {
       dev: {
         script: 'server.js',
         options: {
-          args: [],
-          ignore: ['public/**'],
-          ext: 'js,html',
-          nodeArgs: ['--debug'],
-          delayTime: 1,
+          delay: 1,
           env: {
+            // DEBUG: '*',
             PORT: require('./server/config/config').port
           },
           cwd: __dirname
@@ -100,19 +182,21 @@ module.exports = function(grunt) {
   });
 
   //Load NPM tasks
-  require('load-grunt-tasks')(grunt);
+  // require('load-grunt-tasks')(grunt);
 
   //Making grunt default to force in order not to break the project.
   grunt.option('force', true);
 
   // Build
-  grunt.registerTask('build', ['jshint', 'csslint', 'cssmin', 'uglify']);
+  //grunt.registerTask('build', ['jshint','less:compileCore', 'csslint', 'cssmin', 'uglify']);
+  grunt.registerTask('build', ['clean:build'/*,'jshint'*/,'html2js'/*,'concat'*/,'less:compileCore','copy']);
+  grunt.registerTask('release', ['clean','html2js','uglify','jshint',/*'karma:unit','concat',*/'less:minify','copy']);
 
   //Default task(s).
   if (process.env.NODE_ENV === 'production') {
-    grunt.registerTask('default', ['build', 'concurrent']);
+    grunt.registerTask('default', ['release', 'concurrent']);
   } else {
-    grunt.registerTask('default', ['jshint', 'csslint', 'concurrent']);
+    grunt.registerTask('default', ['build', 'concurrent']);
   }
 
   //Test task.
