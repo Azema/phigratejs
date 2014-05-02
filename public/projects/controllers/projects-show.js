2812f9a2c4f02c072885629cbcea5c6144fd5042
Manuel Hervo <manuel.hervo@gmail.com>
2014-05-02 12:29:13 +0200

    Update Projects show

--- public/projects/controllers/projects-show.js
+++ public/projects/controllers/projects-show.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('phi.projects')
+  .filter('keys', function() {
+    return function(input) {
+      if (!input) {
+        return [];
+      }
+      return Object.keys(input).sort().reverse();
+    };
+  })
   .controller('ProjectsShowCtrl', [
     '$scope',
     '$location',
